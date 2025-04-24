cb40d6d28f3b09079d5e48b860eeefd99d09095e
AlehanR3X <alehandromatias@outlook.com>
2025-04-24 12:21:12 -0500

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# config.py
+## config.py
 api_id = 21214970
 api_hash = '2a300bd6a697a72bcb5fca57b1dec384'
 SESSION_NAME = "telegram_session"  # Nombre de sesión compartida en todos los módulos
@@ -8,8 +8,7 @@ SLEEP_TIME = 35
 TARGET_CHAT_ID = -1002403606655
 DATA_FILE = "datos_extraccion.txt"
 GROUP_CHAT_ID = 2403606655
-GROUP_CHAT_ID2 = -2304417121  # ID del grupo de Telegram
+Rimuru_CHK = 1547217051  # ID del grupo de Telegram
 bot2_username = '@b3authFreeBot'
 bot3_user = "@DuluxeChkFree"
-GROUP_CHAT2_ID = 1547217051
 bot4_user = "@Aki_chkbot"
